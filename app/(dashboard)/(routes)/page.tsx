a5e5b233843873534604d1b7b31ef9c873a33bd7
Jerome Doran <disneyfan9184@gmail.com>
2023-11-24 13:01:18 -0500

    added sidebar and navbar functionality

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <div>
-      <h1 className='text-3xl text-sky-700'>LMS Project</h1>
-      <Button variant='destructive'>Click</Button>
+      <UserButton afterSignOutUrl='/' />
     </div>
   );
 }
