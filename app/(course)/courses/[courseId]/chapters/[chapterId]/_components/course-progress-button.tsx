cf422c826561e5a056df9e60d28d88c83d472c32
Jerome Doran <disneyfan9184@gmail.com>
2024-01-21 12:28:27 -0500

    create course progress button

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useConfettiStore } from '@/hooks/use-confetti-store';
+import { Button } from '@/components/ui/button';
+import { CheckCircle, XCircle } from 'lucide-react';
+import toast from 'react-hot-toast';
+import axios from 'axios';
+
+type CourseProgressButtonProps = {
+  courseId: string;
+  chapterId: string;
+  isCompleted?: boolean;
+  nextChapterId?: string;
+};
+
+export default function CourseProgressButton({
+  courseId,
+  chapterId,
+  isCompleted,
+  nextChapterId,
+}: CourseProgressButtonProps) {
+  const router = useRouter();
+  const confetti = useConfettiStore();
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function onClick() {
+    try {
+      setIsLoading(true);
+      await axios.put(
+        `/api/courses/${courseId}/chapters/${chapterId}/progress`,
+        {
+          isCompleted: !isCompleted,
+        }
+      );
+
+      if (!isCompleted && !nextChapterId) {
+        confetti.onOpen();
+      }
+
+      if (!isCompleted && nextChapterId) {
+        router.push(`/courses/${courseId}/chapters/${nextChapterId}`);
+      }
+
+      toast.success('Course progress updated successfully');
+      router.refresh();
+    } catch (error) {
+      toast.error('Oops...Something went wrong');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const Icon = isCompleted ? XCircle : CheckCircle;
+
+  return (
+    <Button
+      onClick={onClick}
+      disabled={isLoading}
+      type='button'
+      variant={isCompleted ? 'outline' : 'success'}
+      className='w-full md:w-auto'
+    >
+      {isCompleted ? 'Not Completed' : 'Mark as Complete'}
+      <Icon className='h-4 w-4 ml-2' />
+    </Button>
+  );
+}
--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -7,6 +7,8 @@ import VideoPlayer from './_components/video-player';
 import CourseEnrollButton from './_components/course-enroll-button';
 import { Separator } from '@/components/ui/separator';
 import Preview from '@/components/preview';
+import CourseProgress from '@/components/course-progress';
+import CourseProgressButton from './_components/course-progress-button';
 
 export default async function ChapterPage({
   params,
@@ -67,7 +69,12 @@ export default async function ChapterPage({
           <div className='p-4 flex flex-col md:flex-row items-center justify-between'>
             <h2 className='text-2xl font-semibold mb-2'>{chapter.title}</h2>
             {purchase ? (
-              <div>{/* TODO: add progress button */}</div>
+              <CourseProgressButton
+                chapterId={params.chapterId}
+                courseId={params.courseId}
+                nextChapterId={nextChapter?.id}
+                isCompleted={!!userProgress?.isComplete}
+              />
             ) : (
               <CourseEnrollButton
                 courseId={params.courseId}
